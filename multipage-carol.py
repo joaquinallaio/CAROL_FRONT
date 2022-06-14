bc2c8bf30a8d0ebfd0396539d87db85ff34c5e47
Francisco Fischer <francisco.fischer92@gmail.com>
2022-06-14 17:07:48 -0300

    Finished first version of the complete flow end to end

--- multipage-carol.py
+++ multipage-carol.py
@@ -7,9 +7,6 @@ import requests
 from streamlit_lottie import st_lottie
 import pandas as pd
 
-# image = Image.open("../marcacarol.png", "rb")
-
-# st.image(image, caption='Sunrise by the mountains')
 
 def load_lottieurl(url:str):
     r=requests.get(url)
@@ -38,11 +35,7 @@ st_lottie(
 )
 components.html("""<hr style="height:6px;border:none;color:#333;background-color:#FFFFFF;" /> """)
 
-st.markdown(
-        """
-       ## 1️⃣ Carga tu receta?
-        """
-        )
+st.markdown(" ## 1️⃣ Carga tu receta!")
 
 price = None
 df = None
@@ -53,58 +46,37 @@ def get_drugs():
     print("ahi vamos")
     params = {"img_file": image.getvalue()}
     api_url = "https://carol-be-image-s44yr7rzkq-ew.a.run.app/medicines"
+    #api_url = "http://127.0.0.1:8000/medicines"
     res = requests.post(api_url,files=params)
     drugs = res.json()
     spin = True
     return pd.DataFrame(drugs)
 
-
+option = None
 image = st.file_uploader("", type=["png", "jpg", "jpeg", "pdf"])
 if image is not None:
     if not spin:
         with st.spinner('Interpretando receta...'):
             df = get_drugs()
             print(df.keys())
-            options_builder = GridOptionsBuilder.from_dataframe(df)
-            options_builder.configure_selection("single", use_checkbox=True)
-            grid_options = options_builder.build()
-            grid_return = AgGrid(df, grid_options)
-            st.markdown("#" + grid_return)
-            # option = st.selectbox('Seleccioná tus medicamentos', df)
-            # st.markdown(f'Seleccionaste: {option}')
-            # st.markdown(df[df.description == option]['prices'].iloc[0])
+            # options_builder = GridOptionsBuilder.from_dataframe(df)
+            # options_builder.configure_selection("single", use_checkbox=True)
+            # grid_options = options_builder.build()
+            # grid_return = AgGrid(df, grid_options)
+            # st.markdown(grid_return)
+            option = st.selectbox('Seleccioná tus medicamentos', df)
+
     else:
         st.stop()
 
 else:
     print("no hay nada")
 
+# components.html("""<hr style="height:6px;border:none;color:#333;background-color:#FFFFFF;" /> """)
 
+if not option == None:
+    st.markdown("## 2️⃣Confirmá tus medicamentos acá 👇🏽")
+    st.markdown(f'## Seleccionaste: {option}')
+    st.markdown(f"## Precio: $ {df[df.description == option]['prices'].iloc[0]}")
 
-# with st.spinner('Wait for it...'):
-#     time = time.sleep(5)
-
-# if st.button(" 📸 Sacá una foto de tu receta"):
-#     camera = st.camera_input(" 📸 Sacá una foto de tu receta")
-
-def confirmar_medicamento():
-    import streamlit as st
-    st.button("Confirmar medicamento")
-
-# # page_names_to_funcs = {
-#      " 🏠 Home": home,
-#      " 💻 Cargá tu receta desde tu dispositivo": " 💻 Cargá tu receta desde tu dispositivo",
-#      " 📸 Sacá una foto de tu receta": sacar_foto,
-#      " ✅ Confirmar tu medicamento": confirmar_medicamento,
-#         }
-
-
-# barra_azul = st.sidebar.selectbox("Qué te gustaría hacer?", page_names_to_funcs.keys())
-# page_names_to_funcs[barra_azul]()
-
-st.write("#")
-st.write("#")
-components.html("""<hr style="height:6px;border:none;color:#333;background-color:#FFFFFF;" /> """)
-'''
-## 2️⃣Confirmá tus medicamentos acá 👇🏽
-'''
+    st.button("Comprar")
